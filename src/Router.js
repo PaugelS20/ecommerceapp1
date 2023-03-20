5eaf86cf99aff55ec8c9523aaf89f037ab56cc58
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-20 14:06:20 -0500

    adding the router component
    mostly from the textbook with a few updates from func to const and the router is the most current switch to routes and component becomes element

---
+++ src/Router.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Nav from "./Nav";
+import Admin from "./Admin";
+import Main from "./Main";
+import Profile from "./Profile";
+
+const Router = () => {
+	const [current, setCurrent] = useState("home");
+	useEffect(() => {
+		setRoute();
+		window.addEventListener("hashchange", setRoute);
+		return () => window.removeEventListener("hashchange", setRoute); //giving a func back
+	}, []);
+	const setRoute = () => {
+		const location = window.location.href.split("/");
+		const pathname = location[location.length - 1];
+		console.log("pathname: ", pathname);
+		setCurrent(pathname ? pathname : "home");
+	}
+	return (
+		<HashRouter>
+			<Nav current={current} />
+			<Routes>
+				<Route exact path="/" element={<Main />} />
+				<Route path="/admin" element={<Admin />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route element={<Main />} />
+			</Routes>
+		</HashRouter>
+	);
+}
+
+export default Router;
