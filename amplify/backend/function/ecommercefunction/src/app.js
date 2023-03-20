5692175d3dbdd46f294d52a9db732e5ae5e75079
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-20 01:22:15 -0500

    Adding functions to check the users group
    and add delete func

--- amplify/backend/function/ecommercefunction/src/app.js
+++ amplify/backend/function/ecommercefunction/src/app.js
@@ -29,12 +29,48 @@ AWS.CognitoIdentityServiceProvider({
    adding the category
 *  This will also be available in the file itself, commented out at the top
 */
-var userpoolId = process.env.AUTH_ECOMMERCEAPP1122DA288_USERPOOLID
+var userpoolId = process.env.AUTH_ECOMMERCEAPP1122DA288_USERPOOLID;
 
 // DynamoDB configuration
-const region = process.env.REGION
-const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME
-const docClient = new AWS.DynamoDB.DocumentClient({region})
+const region = process.env.REGION;
+const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME;
+const docClient = new AWS.DynamoDB.DocumentClient({region});
+
+const getGroupsForUser = async(event) => {
+  let userSub =
+    event
+      .requestContext
+      .identity
+      .cognitoAuthenticationProvider
+      .split(':CognitoSignIn:')[1];
+  let userParams = {
+    UserPoolId: userpoolId,
+    Filter: `sub = "${userSub}"`,
+  }
+  let userData = await cognito.listUsers(userParams).promise()
+  const user = userData.Users[0];
+  var groupParams = {
+    UserPoolId: userpoolId,
+    Username: user.Username
+  }
+  const groupData = await cognito.adminListGroupsForUser(groupParams).promise()
+  return groupData;
+}
+
+const canPerformAction = async(event, group) => {
+  return new Promise(async (resolve, reject) => {
+    if (!event.requestContext.identity.cognitoAuthenticationProvider) {
+      return reject();
+    }
+    const groupData = await getGroupsForUser(event);
+    const groupsForUser = groupData.Groups.map(group => group.GroupName);
+    if (groupsForUser.includes(group)) {
+      resolve()
+    } else {
+      reject('user not in group, cannot perform action..');
+    }
+  })
+}
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -57,10 +93,24 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 
-app.get('/products', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+app.get('/products', async function(req, res) {
+  try {
+    const data = await getItems()
+    res.json({ data: data });
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
+
+const getItems = async() => {
+  var params = { TableName: ddb_table_name }
+  try {
+    const data = await docClient.scan(params).promise();
+    return data;
+  } catch (err) {
+    return err;
+  }
+}
 
 app.get('/products/*', function(req, res) {
   // Add your code here
@@ -71,9 +121,21 @@ app.get('/products/*', function(req, res) {
 * Example post method *
 ****************************/
 
-app.post('/products', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+app.post('/products', async function(req, res) {
+  const { body } = req;
+  const { event } = req.apiGateway;
+  try {
+    await canPerformAction(event, 'Admin')
+    const input = { ...body, id: uuid() }
+    var params = {
+      TableName: ddb_table_name,
+      Item: input
+    }
+    await docClient.put(params).promise()
+    res.json({ success: 'item saved to database..' })
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 
 app.post('/products/*', function(req, res) {
@@ -99,9 +161,19 @@ app.put('/products/*', function(req, res) {
 * Example delete method *
 ****************************/
 
-app.delete('/products', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+app.delete('/products', async function(req, res) {
+  const { event } = req.apiGateway;
+  try {
+    await canPerformAction(event, 'Admin')
+    var params = {
+      TableName : ddb_table_name,
+      Key: { id: req.body.id }
+    }
+    await docClient.delete(params).promise()
+    res.json({ success: 'successfully deleted item' })
+  } catch (err) {
+    res.json({ error: err });
+  }
 });
 
 app.delete('/products/*', function(req, res) {
