ac1cd9e770543f3367ccf976f6185e1ec2f51a5f
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-20 15:06:45 -0500

    making amplify a named export
    and changing the router output to modern syntax

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
 
-import "antd/dist/antd.css";
-import Amplify from "aws-amplify";
+// import "antd/dist/antd.css";
+import { Amplify } from "aws-amplify";
 import config from "./aws-exports";
 Amplify.configure(config);
 
-ReactDOM.render(<Router />, document.getElementById("root"));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Router />);
