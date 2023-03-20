8bc69a4c65f7df3a8ea9a6f3c647c7451021e199
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-20 15:42:44 -0500

    went back to old way
    wasn't sure what I should do so changed it back

--- src/Nav.js
+++ src/Nav.js
@@ -4,9 +4,11 @@ import { Menu } from "antd";
 import { HomeOutlined, UserOutlined, ProfileOutlined } from "@ant-design/icons";
 import { Hub } from "aws-amplify";
 import checkUser from "./checkUser";
+import "./App.css";
 
 const Nav = ({ current }) => {
 	const [user, updateUser] = useState({});
+	console.log(user);
 	useEffect(() => {
 		checkUser(updateUser);
 		Hub.listen("auth", (data) => {
@@ -17,62 +19,54 @@ const Nav = ({ current }) => {
 		});
 	}, []);
 
-
-		const items = [
-		{
-			label: <Link to={`/`}>Home</Link>,
-			key: "home",
-			icon: <HomeOutlined />,
-		},
-		{
-			label: <Link to={`/profile`}>Profile</Link>,
-			key: "profile",
-			icon: <UserOutlined />, /*<ProfileOutlined />*/
-		},
-		user.isAuthorized && ({
-			label: <Link to={`/admin`}>Admin</Link>,
-			key: "admin",
-			icon: <ProfileOutlined />,
-		}),
-	];
-
-	return <Menu selectedKeys={[current]} mode="horizontal" items={items} />;
-
-
-	// return (
-	// 	<div>
-	// 		<Menu selectedKeys={[current]} mode="horizontal">
-	// 			<Menu.Item key="home">
-	// 				<Link to={`/`}>
-	// 					<HomeOutlined />
-	// 					Home
-	// 				</Link>
-	// 			</Menu.Item>
+	return (
+		<div>
+			<Menu selectedKeys={[current]} mode="horizontal">
+				<Menu.Item key="home">
+					<Link to={`/`}>
+						<HomeOutlined /> Home
+					</Link>
+				</Menu.Item>
 				
-    //             <Menu.Item key="profile">
-	// 				<Link to="/profile">
-	// 					<UserOutlined />
-	// 					Profile
-	// 				</Link>
-	// 			</Menu.Item>
+                <Menu.Item key="profile">
+					<Link to="/profile">
+						<UserOutlined /> Profile
+					</Link>
+				</Menu.Item>
 				
-    //             {user.isAuthorized && (
-	// 				<Menu.Item key="admin">
-	// 					<Link to="/admin">
-	// 						<ProfileOutlined />
-	// 						Admin
-	// 					</Link>
-	// 				</Menu.Item>
-	// 			)}
-	// 		</Menu>
-	// 	</div>
-	// );
+                {user.isAuthorized && (
+					<Menu.Item key="admin">
+						<Link to="/admin">
+							<ProfileOutlined /> Admin
+						</Link>
+					</Menu.Item>
+				)}
+			</Menu>
+		</div>
+	);
 };
 
 export default Nav;
 
 
 // const Nav = () => {
+// 	const items = [
+// 		{
+// 			label: <Link to={`/`}>Home</Link>,
+// 			key: "home",
+// 			icon: <HomeOutlined />,
+// 		},
+// 		{
+// 			label: <Link to={`/profile`}>Profile</Link>,
+// 			key: "profile",
+// 			icon: <UserOutlined />, /*<ProfileOutlined />*/
+// 		},
+// 		{user.isAuthorized && (
+// 			label: <Link to={`/admin`}>Admin</Link>,
+// 			key: "admin",
+// 			icon: <ProfileOutlined />,
+// 		)},
+// 	];
 
 // 	const [current] = useState();
 
