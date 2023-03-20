383e80a3517f8817b47208a584cf983e2ad217e0
Sam Paugel <shpaugel@madisoncollege.edu>
2023-03-20 13:54:07 -0500

    adding Profile from the text book two changes
    one the function to const
    two the modern signout for amplify because
    amplifysignout doesn't work anymore

---
+++ src/Profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+import { withAuthenticator} from "@aws-amplify/ui-react";
+
+const Profile = ({ signOut }) => {
+	return (
+		<div style={containerStyle}>
+			<button onClick={signOut}>Sign out</button>
+		</div>
+	);
+}
+
+const containerStyle = {
+	width: 400,
+	margin: "20px auto",
+};
+
+export default withAuthenticator(Profile);
